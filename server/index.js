661f333df64062acc9ef2227637160e57db05cfc
Hemu21 <rajva5727@gmail.com>
2025-06-08 16:04:39 +0530

    feat: add CORS support and enhance recipe response structure

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const recipesRouter = require("./routes/recipes");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors())
 app.use("/recipes", recipesRouter);
 
 app.get("/", (req, res) => {
